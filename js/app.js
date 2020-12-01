89ba67bb62497eb2df38bcd8672e0918f32da1c7
sixhours <calobee@gmail.com>
2020-12-01 09:30:48 -0500

    Update comments, minor spacing cleanup

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 let game = new Game();
-const button = document.getElementById('begin-game');
+const button = document.getElementById( 'begin-game' );
 
 /** 
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-button.addEventListener('click', function() {
+button.addEventListener( 'click', function() {
     game.startGame();
     this.style.display = 'none';
-    document.getElementById('play-area').style.opacity = '1';
-});
+    document.getElementById( 'play-area' ).style.opacity = '1';
+} );
 
+/** 
+ * Add an event listener to the Replay game button if the game is over
+ */
 if ( game.isGameOver ) {
-    const replay = document.getElementById('replay');
-    replay.addEventListener('click', function() {
+    const replay = document.getElementById( 'replay' );
+    replay.addEventListener( 'click', function() {
         window.location.reload();
-    });
+    } );
 }
 
-document.addEventListener( 'keydown', function(event) {
+/** 
+ * Listen for keyboard actions to move tokens
+ */
+document.addEventListener( 'keydown', function( event ) {
     game.handleKeyDown( event );
 } );
